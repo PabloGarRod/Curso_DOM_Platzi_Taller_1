64c79a3d888007b428f427890065a2bdae730a44
Pablo <pablogarcirodriguez@gmail.com>
2022-08-13 12:51:41 +0200

    Clase 12: descargando información y creando nodos

--- src/index.js
+++ src/index.js
@@ -1,7 +1,32 @@
 const url = "https://platzi-avo.vercel.app/api/avo"
 
-//web api
-//Conectarnos al server
-//Procesar la respuesta y convertirla en JSON
-//JSON --> Data --> Rendereizar
-window.fetch(url)
+// //web api
+// //Conectarnos al server
+// window.fetch(url)
+// //Procesar la respuesta y convertirla en JSON
+// .then((respuesta)=>respuesta.json())
+// //JSON --> Data --> Rendereizar info browser
+// .then((respuestaJson)=>{
+//     respuestaJson.data.forEach(item => {
+//         console.log(item.name)
+//     });
+// });
+
+
+const datos = await fetch(url);
+const respuestaJson =  await datos.json();
+const todos = []
+respuestaJson.data.forEach(item => {
+    //crear imagen
+    const image = document.createElement('img')
+    //crear titulo
+    const title = document.createElement('h2')
+    //crear precio
+    const price = document.createElement('div')
+
+    const container = document.createElement('div')
+    container.append(image,title,price)
+    todos.push(container)
+});
+
+document.body.append(...todos)
