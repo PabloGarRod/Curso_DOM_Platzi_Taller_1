ceb8947ce2366b762904a2947fb482f37b159a1f
Pablo <pablogarcirodriguez@gmail.com>
2022-08-14 17:41:49 +0200

    Clase 13: Enriqueciendo la información

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
-const url = "https://platzi-avo.vercel.app/api/avo"
+const baseUrl = "https://platzi-avo.vercel.app"
+const appNode = document.querySelector('#app')
 
 // //web api
 // //Conectarnos al server
@@ -13,20 +14,23 @@ const url = "https://platzi-avo.vercel.app/api/avo"
 // });
 
 
-const datos = await fetch(url);
+const datos = await fetch(`${baseUrl}/api/avo`);
 const respuestaJson =  await datos.json();
 const todos = []
 respuestaJson.data.forEach(item => {
     //crear imagen
     const image = document.createElement('img')
+    image.src = `${baseUrl}${item.image}`;
     //crear titulo
     const title = document.createElement('h2')
+    title.textContent = item.name;
     //crear precio
     const price = document.createElement('div')
+    price.textContent = item.price;
 
     const container = document.createElement('div')
     container.append(image,title,price)
     todos.push(container)
 });
 
-document.body.append(...todos)
+appNode.append(...todos)
